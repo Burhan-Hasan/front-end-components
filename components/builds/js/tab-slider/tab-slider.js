787c290c651cc8e44b198dbd1496736297828f71
Burhan <burhan.hasan.dev@gmail.com>
2017-07-16 15:16:40 +0400

    base functionality

--- components/builds/js/tab-slider/tab-slider.js
+++ components/builds/js/tab-slider/tab-slider.js
@@ -1,1 +1,27 @@
-var x = 5;
+var tabSlider = (function () {
+    /**
+     *
+     */
+    function tabSlider(_a) {
+        var container = _a.container;
+        this.initEvents(container);
+    }
+    tabSlider.prototype.initEvents = function (container) {
+        container.onclick = this.tabClick;
+    };
+    //EVENTS
+    tabSlider.prototype.tabClick = function (e) {
+        var target = e.target;
+        var tabBtn = target.closest('.tab-button');
+        if (tabBtn) {
+            var tabFor = tabBtn.getAttribute('tab-for');
+            var content = document.getElementById(tabFor);
+            var contents = content.parentElement.children;
+            for (var i = 0; i < contents.length; i++) {
+                contents[i].removeAttribute('active');
+            }
+            content.setAttribute('active', '');
+        }
+    };
+    return tabSlider;
+}());
--- components/tab-slider/tab-slider.ts
+++ components/tab-slider/tab-slider.ts
@@ -1,1 +1,28 @@
-var x=5;
+class tabSlider {
+    /**
+     *
+     */
+    constructor({ container }) {
+        this.initEvents(container);
+    }
+
+    initEvents(container) {
+        container.onclick = this.tabClick;
+    }
+
+
+    //EVENTS
+    tabClick(e) {
+        let target = e.target;
+        let tabBtn = target.closest('.tab-button');
+        if (tabBtn) {
+            let tabFor = tabBtn.getAttribute('tab-for');
+            let content = document.getElementById(tabFor);
+            let contents = content.parentElement.children;
+            for (let i = 0; i < contents.length; i++) {
+                contents[i].removeAttribute('active');
+            }
+            content.setAttribute('active', '');
+        }
+    }
+}
